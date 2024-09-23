3939dae16f6b4b98d441ce034ff0ad297ffd14f8
jackyhsu <m0972220819@gmail.com>
2024-09-23 22:47:14 +0800

    update stack.js & package.json which has ESM Module

--- week-02/main.js
+++ week-02/main.js
@@ -8,5 +8,17 @@ stack.print();
 stack.push(5);
 stack.push(8);
 stack.print();
+console.log(stack.isEmpty());
+
+console.log(stack.peek());
+
+console.log(stack.pop());
+
+console.log(stack.size());
+
+stack.print();
+
+stack.clear();
+console.log(stack.isEmpty());
 
 // TODO: 應該還要做哪些測試，以驗證自己開發的 stack 是沒有問題的？
--- week-02/stack.js
+++ week-02/stack.js
@@ -2,6 +2,8 @@
 // 完成以下 TODO 的部分，並且以 Module 的方式匯出 (ESM)
 export default class Stack {
 	// TODO: # 有特別的意思嗎？請以註解回覆。
+  // 透過前綴 # 符號來創建一個物件的私有屬性
+  // 只能在物件內部被使用，而無法透過外部存取，以此來保護內部資料封裝
   #items;
 
   constructor() {
@@ -11,37 +13,51 @@ export default class Stack {
   // 在 stack 頂部加入元素i
   push(element) {
 		// TODO
+    this.#items.push(element);
   }
 
   // 移除並回傳 stack 頂部的元素
   pop() {
 		// TODO
+    return this.#items.pop();
   }
 
   // 回傳 stack 頂部的元素，但不移除它
   peek() {
     // TODO
+    let topIndex = this.#items.length - 1
+    return this.#items[topIndex];
   }
 
   // 檢查 stack 是否為空
   isEmpty() {
     // TODO
+    if (this.#items.length === 0)
+      return true;
+    else
+      return false;
   }
 
   // 回傳 stack 中元素的個數
   size() {
     // TODO
+    let elements = this.#items.length;
+    return elements;
   }
 
   // 清空 stack 
   clear() {
     // TODO
+    this.#items = [];
   }
 
   // 印出 stack 內容
   print() {
-    // TODO
+    for (let i = 0; i < this.#items.length; i++){
+      console.log(this.#items[i]);
+    }  
   }
+
 }
-​
+
 // 測試程式:
